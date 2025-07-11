3041984026558249f5c5c50feafd63d9a6563568
nicolamarino <nicolamarino527@gmail.com>
2025-07-11 15:41:58 +0200

    create project

---
+++ src/main/java/org/project/spring/videogame_page/videogame_page_spring_backoffice/VideogamePageSpringBackofficeApplication.java
@@ -0,0 +1,13 @@
+package org.project.spring.videogame_page.videogame_page_spring_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamePageSpringBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamePageSpringBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/spring/videogame_page/videogame_page_spring_backoffice/VideogamePageSpringBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.spring.videogame_page.videogame_page_spring_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamePageSpringBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
