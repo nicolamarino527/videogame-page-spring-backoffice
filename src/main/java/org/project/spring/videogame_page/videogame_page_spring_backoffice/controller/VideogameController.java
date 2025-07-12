311c5307eb05e19915204788925db272d1c2a3d9
nicolamarino <nicolamarino527@gmail.com>
2025-07-12 16:13:26 +0200

    [feat] add home controller and navbar fragment, csrf disabled.

---
+++ src/main/java/org/project/spring/videogame_page/videogame_page_spring_backoffice/controller/VideogameController.java
@@ -0,0 +1,10 @@
+package org.project.spring.videogame_page.videogame_page_spring_backoffice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class VideogameController {
+
+}
