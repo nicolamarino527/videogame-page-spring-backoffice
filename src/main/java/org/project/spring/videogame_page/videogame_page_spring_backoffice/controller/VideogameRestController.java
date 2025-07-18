62a622a34d16e25b471b46e1da42547535df5dab
nicolamarino <nicolamarino527@gmail.com>
2025-07-18 20:53:22 +0200

    [feat] add cross origin.

--- src/main/java/org/project/spring/videogame_page/videogame_page_spring_backoffice/controller/VideogameRestController.java
+++ src/main/java/org/project/spring/videogame_page/videogame_page_spring_backoffice/controller/VideogameRestController.java
@@ -7,11 +7,13 @@ import org.project.spring.videogame_page.videogame_page_spring_backoffice.model.
 import org.project.spring.videogame_page.videogame_page_spring_backoffice.service.VideogameService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/videogames")
 public class VideogameRestController {
